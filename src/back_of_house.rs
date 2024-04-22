7c456c7105061dd2d1c9390c10150ae7c31743c0
Coufal, Jan <jan.coufal@oracle.com>
2024-04-22 21:34:37 +0200

    vectors

--- src/back_of_house.rs
+++
@@ -1,6 +0,0 @@
-pub mod breakfast;
-pub use breakfast::Breakfast;
-
-pub fn say_breakfast() {
-    println!("Breakfast!");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,72 @@
-use crate::back_of_house::say_breakfast;
-
-mod front_of_house;
-mod back_of_house;
-
 fn main() {
-    front_of_house::greetings("enter");
-    front_of_house::hosting::add_to_waitlist();
-    front_of_house::serving::take_order();
-    front_of_house::serving::take_payment_and_eat_again();
-    front_of_house::serving::take_payment_and_eat_again2();
+    let mut v = vec![1, 2, 3, 4, 5];
 
-    let breakfast = back_of_house::breakfast::Breakfast::summer("White bread");
-    println!("{}", breakfast.to_string());
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
 
-    let mut breakfast = back_of_house::Breakfast::summer("Rye");
-    println!("{}", breakfast.to_string());
-    breakfast.toast = String::from("Wheat");
-    println!("{}", breakfast.to_string());
+    let another_third: Option<&i32> = v.get(2);
+    match another_third {
+        None => println!("There is no third element."),
+        Some(some_third) => println!("Another third element is {some_third}"),
+    }
 
-    say_breakfast();
+    v[2] *= 10;
+    // drop(third);
 
-    use_of_use();
+    let third_2: &i32 = &v[2];
+    println!("The third element is {third_2}");
+
+    let another_third_2: Option<&i32> = v.get(2);
+    match another_third_2 {
+        None => println!("There is no third element."),
+        Some(some_third) => println!("Another third element is {some_third}"),
+    }
+
+    // println!("first third is {third}");
+    vec_games();
+    vec_of_enum();
 }
 
-fn use_of_use() {
-    println!("use of use");
-    use front_of_house::serving;
-    serving::take_order();
-    serving::take_payment_and_eat_again();
+fn vec_games() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    println!("{:?}", v);
+
+    for i in &mut v {
+        *i *= 10;
+    }
+    println!("{:?}", v);
+
+    for i in &mut v {
+        *i /= 5;
+    }
+    println!("{:?}", v);
+
+    for i in &v {
+        print!("{i}..")
+    }
+    println!("{:?}", v);
+}
+
+fn vec_of_enum() {
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    for r in &row {
+        print!("r = {{");
+        match r {
+            SpreadsheetCell::Int(i) => print!("Int={i:?}"),
+            SpreadsheetCell::Float(f) => print!("Float={f:?}"),
+            SpreadsheetCell::Text(t) => print!("Text={t:?}"),
+        }
+        println!("}}");
+    }
 }
