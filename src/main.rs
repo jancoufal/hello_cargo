bb062160fa05eca125212bae630e89783e41a8a5
Coufal, Jan <jan.coufal@oracle.com>
2024-04-22 22:18:02 +0200

    strings basic

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,22 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
+    let s = "🧻🤪👻🛀";
 
-    let third: &i32 = &v[2];
-    println!("The third element is {third}");
+    dbg!(s);
+    dbg!(s.len());
+    dbg!(&s);
+    dbg!(&s.len());
+    dbg!(&s[..]);
+    dbg!(&s[..4]);
+    dbg!(&s[4..8]);
+    dbg!(&s[8..]);
+    // dbg!(&s[1..5]); // panic!
 
-    let another_third: Option<&i32> = v.get(2);
-    match another_third {
-        None => println!("There is no third element."),
-        Some(some_third) => println!("Another third element is {some_third}"),
+    for c in s.chars() {
+        print!("{c}..")
     }
+    println!();
 
-    v[2] *= 10;
-    // drop(third);
-
-    let third_2: &i32 = &v[2];
-    println!("The third element is {third_2}");
-
-    let another_third_2: Option<&i32> = v.get(2);
-    match another_third_2 {
-        None => println!("There is no third element."),
-        Some(some_third) => println!("Another third element is {some_third}"),
-    }
-
-    // println!("first third is {third}");
-    vec_games();
-    vec_of_enum();
-}
-
-fn vec_games() {
-    let mut v = vec![1, 2, 3, 4, 5];
-    println!("{:?}", v);
-
-    for i in &mut v {
-        *i *= 10;
-    }
-    println!("{:?}", v);
-
-    for i in &mut v {
-        *i /= 5;
-    }
-    println!("{:?}", v);
-
-    for i in &v {
-        print!("{i}..")
-    }
-    println!("{:?}", v);
-}
-
-fn vec_of_enum() {
-    enum SpreadsheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String),
-    }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-
-    for r in &row {
-        print!("r = {{");
-        match r {
-            SpreadsheetCell::Int(i) => print!("Int={i:?}"),
-            SpreadsheetCell::Float(f) => print!("Float={f:?}"),
-            SpreadsheetCell::Text(t) => print!("Text={t:?}"),
-        }
-        println!("}}");
-    }
+    dbg!(&s.chars().count());
+    dbg!(&s.chars().nth(3));
+    dbg!(&s.chars().nth(4));
 }
