23e4d7ff5ad0ac861c145b4cb6daedc5f56545bf
Coufal, Jan <jan.coufal@oracle.com>
2024-05-14 23:31:15 +0200

    hash maps and strings

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ fn main() {
     hm1(&scores);
 
     games();
+
+    tests();
 }
 
 fn hm1(m: &HashMap<String, i32>) {
@@ -56,3 +58,83 @@ fn games() {
         println!("{:?}", map);
     }
 }
+
+fn tests() {
+    task_1();
+    task_2();
+    task_2_chat_gpt();
+}
+
+fn task_1() {
+    // Given a list of integers, use a vector and return the median
+    // (when sorted, the value in the middle position) and mode (the
+    // value that occurs most often; a hash map will be helpful here)
+    // of the list.
+    let randoms = vec![11,33,55,77,99,22,44,66,88];
+    let mut sorted = randoms.clone();
+    sorted.sort();
+
+    let median_index = sorted.len() / 2;
+    let median = sorted.get(median_index);
+    println!("median of {:?} is {:?}", randoms, median);
+}
+
+fn task_2() {
+    let input = "Convert strings to pig latin. The first consonant of \
+    each word is moved to the end of the word and “ay” is added, so “first” \
+    becomes “irst-fay.” Words that start with a vowel have “hay” added to \
+    the end instead (“apple” becomes “apple-hay”). Keep in mind the details \
+    about UTF-8 encoding!".to_string();
+
+    let vowels = "aeiouyAEIOUY".to_string();
+
+    input.split_whitespace().for_each(|w| {
+        match w.chars().nth(0) {
+            None => {}
+            Some(c) => {
+                if vowels.contains(c) {
+                    // println!("Starts with vowel '{}':", c);
+                    print!("{w}-hay");
+                }
+                else if c.is_alphabetic() {
+                    // println!("Starts with consonant '{}'", c);
+                    print!("{}-{c}ay", &w[1..]);
+                }
+                else {
+                    // println!("Starts with unknown '{}'", c);
+                    print!("{w}");
+                }
+            }
+        }
+        // println!("...{:?} [{:?}]", w, w.chars().nth(0));
+        print!(" ");
+    });
+
+    // println!("{input}");
+    println!();
+}
+
+fn task_2_chat_gpt() {
+    let input = "Convert strings to pig latin. The first consonant of \
+    each word is moved to the end of the word and “ay” is added, so “first” \
+    becomes “irst-fay.” Words that start with a vowel have “hay” added to \
+    the end instead (“apple” becomes “apple-hay”). Keep in mind the details \
+    about UTF-8 encoding!";
+
+    let vowels = "aeiouyAEIOUY";
+
+    input.split_whitespace().for_each(|w| {
+        if let Some(c) = w.chars().next() {
+            if vowels.contains(c) {
+                print!("{}-hay", w);
+            } else if c.is_alphabetic() {
+                print!("{}-{}ay", &w[1..], c);
+            } else {
+                print!("{}", w);
+            }
+        }
+        print!(" ");
+    });
+    println!();
+}
+
