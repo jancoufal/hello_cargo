19e43087c7e8463fa8cb3766cbf226d49604efa8
Coufal, Jan <jan.coufal@oracle.com>
2024-08-06 23:01:34 +0200

    reference counting

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,35 @@
+use std::rc::Rc;
+
+use crate::List::{Cons, Nil};
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
 fn main() {
-    println!("closure_1");
-    closure_1();
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 
-    println!("closure_2");
-    closure_2();
+    print!("a = ");
+    print_cons(&a);
 
-    println!("closure_3");
-    closure_3();
+    print!("b = ");
+    print_cons(&b);
 
-    println!("closure_4");
-    closure_4();
+    print!("c = ");
+    print_cons(&c);
 }
 
-fn closure_1() {
-    let list = vec![1, 2, 3];
-    println!("Before defining closure: {list:?}");
-
-    let only_borrows = || println!("From closure: {list:?}");
-
-    println!("Before calling closure: {list:?}");
-    only_borrows();
-    println!("After calling closure: {list:?}");
-}
-
-fn closure_2() {
-    let mut list = vec![1, 2, 3];
-    println!("Before defining closure: {list:?}");
-
-    let mut borrows_mutably = || {
-        println!("from borrows_mutably closure (1): {list:?}");
-        list.push(7);
-        println!("from borrows_mutably closure (2): {list:?}");
-    };
-
-    borrows_mutably();
-    // println!("After calling closure (1): {list:?}");
-
-    borrows_mutably();
-    println!("After calling closure (2): {list:?}");
-}
-
-fn closure_3() {
-    let mut list = vec![1, 2, 3];
-    println!("Before defining closure: {list:?}");
-
-    let mut move_ownership = move || {
-        println!("from borrows_mutably closure (1): {list:?}");
-        list.push(7);
-        println!("from borrows_mutably closure (2): {list:?}");
-    };
-
-    move_ownership();
-    // println!("After calling closure (1): {list:?}");
-
-    move_ownership();
-    // println!("After calling closure (2): {list:?}");
-}
-
-// "FnOnce", "FnMut", "Fn" tests
-fn closure_4() {
-    c_fn_once_call();
-}
-
-fn c_fn_once<T>(f: impl FnOnce() -> T) -> T {
-    f()
-}
-
-fn c_fn_once_call() {
-    let s1 = "abc".to_string();
-    println!("s1 = {s1:?}");
-
-    let s2 = c_fn_once(|| s1.to_string());
-    println!("s1 = {s1:?}");
-    println!("s2 = {s2:?}");
+fn print_cons(l: &List) {
+    match l {
+        Cons(v, ll) => {
+            print!("{}..", v);
+            print_cons(ll);
+        }
+        Nil => {
+            println!("/");
+        }
+    }
 }
