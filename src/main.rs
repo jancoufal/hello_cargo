9a71ce31992d40d18db5ce597c831a6b901458e6
Coufal, Jan <jan.coufal@oracle.com>
2024-08-06 22:47:02 +0200

    closure games

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,76 @@
-fn  add_one_v1   (x: u32) -> u32 { x + 1 }
-
 fn main() {
+    println!("closure_1");
+    closure_1();
 
-    let add_one_v2 = |x: u32| -> u32 { x + 1 };
-    let add_one_v3 = |x|             { x + 1 };
-    let add_one_v4 = |x|               x + 1  ;
+    println!("closure_2");
+    closure_2();
 
-    println!("v1: {}", add_one_v1(0));
-    println!("v2: {}", add_one_v2(1));
-    println!("v3: {}", add_one_v3(2));
-    println!("v4: {}", add_one_v4(3));
+    println!("closure_3");
+    closure_3();
+
+    println!("closure_4");
+    closure_4();
+}
+
+fn closure_1() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
+}
+
+fn closure_2() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let mut borrows_mutably = || {
+        println!("from borrows_mutably closure (1): {list:?}");
+        list.push(7);
+        println!("from borrows_mutably closure (2): {list:?}");
+    };
+
+    borrows_mutably();
+    // println!("After calling closure (1): {list:?}");
+
+    borrows_mutably();
+    println!("After calling closure (2): {list:?}");
+}
+
+fn closure_3() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let mut move_ownership = move || {
+        println!("from borrows_mutably closure (1): {list:?}");
+        list.push(7);
+        println!("from borrows_mutably closure (2): {list:?}");
+    };
+
+    move_ownership();
+    // println!("After calling closure (1): {list:?}");
+
+    move_ownership();
+    // println!("After calling closure (2): {list:?}");
+}
+
+// "FnOnce", "FnMut", "Fn" tests
+fn closure_4() {
+    c_fn_once_call();
+}
+
+fn c_fn_once<T>(f: impl FnOnce() -> T) -> T {
+    f()
+}
+
+fn c_fn_once_call() {
+    let s1 = "abc".to_string();
+    println!("s1 = {s1:?}");
+
+    let s2 = c_fn_once(|| s1.to_string());
+    println!("s1 = {s1:?}");
+    println!("s2 = {s2:?}");
 }
