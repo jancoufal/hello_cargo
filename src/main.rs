150a4f24a9c0ff6d0b506ffaac2abbfd2a1af35a
Coufal, Jan <jan.coufal@oracle.com>
2024-09-22 16:02:52 +0200

    threads - move

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let counting_thread = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_secs(1));
-        }
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("v: {v:?}");
     });
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_secs_f32(1.33));
-    }
+    // drop(v); // oh no!
 
-    // counting_thread.join().unwrap();
+    handle.join().unwrap();
 }
