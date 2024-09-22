33a38b72d21a38aa33a6222b4e588ca15d06a153
Coufal, Jan <jan.coufal@oracle.com>
2024-09-22 15:20:09 +0200

    reference counting

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,51 @@ struct Node {
     children: RefCell<Vec<Rc<Node>>>,
 }
 
+// 1) leaf strong = 1, weak = 0
+// 2) branch strong = 1, weak = 1
+// 3) leaf strong = 2, weak = 0
+// 4) leaf parent = None
+// 5) leaf strong = 1, weak = 0
+
 fn main() {
     let leaf = Rc::new(Node {
         value: 3,
         parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![])
+        children: RefCell::new(vec![]),
     });
 
-    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "1) leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 
-    let branch = Rc::new(Node {
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
 
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
-    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
+        println!(
+            "2) branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "3) leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("4) leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "5) leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
