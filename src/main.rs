2aae90e48d5a6ff9f24e7a0ce619ece67243f212
Coufal, Jan <jan.coufal@oracle.com>
2024-10-09 22:47:30 +0200

    blog post

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,14 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
+use hello_cargo::Post;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let mut post = Post::new();
 
-    let tx1 = tx.clone();
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
 
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    post.request_review();
+    assert_eq!("", post.content());
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {received}");
-    }
-
-    // --snip--
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
